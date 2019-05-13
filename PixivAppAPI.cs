b305072f7b9d208d536ea19319b8630673ee5823
tobiichiamane <tobiichiamane@outlook.com>
2019-05-13 09:57:14 +0800

    完成UserFollower

--- PixivAppAPI.cs
+++ PixivAppAPI.cs
@@ -314,5 +314,20 @@ namespace PixivCS
             var res = await RequestCall("GET", url, Query: query, RequireAuth: RequireAuth);
             return JsonObject.Parse(await GetResponseString(res));
         }
+
+        //Followers用户列表
+        public async Task<JsonObject> UserFollower(string UserID, string Restrict = "public",
+            string Offset = null, bool RequireAuth = true)
+        {
+            string url = "https://app-api.pixiv.net/v1/user/follower";
+            List<(string, string)> query = new List<(string, string)>
+            {
+                ("user_id", UserID),
+                ("restrict", Restrict)
+            };
+            if (!string.IsNullOrEmpty(Offset)) query.Add(("offset", Offset));
+            var res = await RequestCall("GET", url, Query: query, RequireAuth: RequireAuth);
+            return JsonObject.Parse(await GetResponseString(res));
+        }
     }
 }
