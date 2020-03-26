093bd114b1ad0b9873a8fc105117bb63025217e4
tobiichiamane <tobiichiamane@outlook.com>
2020-03-26 17:17:25 +0800

    更新证书细节

--- PixivBaseAPI.cs
+++ PixivBaseAPI.cs
@@ -61,12 +61,12 @@ namespace PixivCS
         };
         public Dictionary<string, string> TargetSNs { get; set; } = new Dictionary<string, string>()
         {
-            {"210.140.131.188","281941D074A6D4B07B72D729" },
+            {"210.140.131.188","35C1AFCF189CA529709BDC9A" },
             {"210.140.92.142","2387DB20E84EFCF82492545C" }
         };
         public Dictionary<string, string> TargetTPs { get; set; } = new Dictionary<string, string>()
         {
-            {"210.140.131.188","352FCC13B920E12CD15F3875E52AEDB95B62972B" },
+            {"210.140.131.188","D684756F1EF93CED5139AA9983FEFB6EE25EA820" },
             {"210.140.92.142","F4A431620F42E4D10EB42621C6948E3CD5014FB0" }
         };
 
